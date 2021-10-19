65105976b6c95865e06cc4b1dd61162c700e5946
Oppong200 <oppongnathaniel14@gmail.com>
2021-10-19 10:57:40 +0000

    validation of inputs done Image cropper introduced

--- lib/views/auth/create_account_view.dart
+++ lib/views/auth/create_account_view.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:unicons/unicons.dart';
 
@@ -12,7 +14,7 @@ class CreateAccountView extends StatefulWidget {
 }
 
 class _CreateAccountViewState extends State<CreateAccountView> {
-  final GlobalKey<FormState>_formKey =GlobalKey();
+  final GlobalKey<FormState> _formKey = GlobalKey();
 
   final TextEditingController _nameController = TextEditingController();
 
@@ -24,15 +26,36 @@ class _CreateAccountViewState extends State<CreateAccountView> {
 
   File? _imageFile;
 
-  Future selectImage({ImageSource imageSource = ImageSource.camera}) async{
-    
-          XFile? selectedFile = await _imagePicker.pickImage(source: imageSource);
+  final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
 
-    return File(selectedFile!.path);
+  Future selectImage({ImageSource imageSource = ImageSource.camera}) async {
+    XFile? selectedFile = await _imagePicker.pickImage(source: imageSource);
+
+    File? croppedFile = await ImageCropper.cropImage(
+        sourcePath: selectedFile!.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: const AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: const IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ));
+
+    setState(() {
+      _imageFile = croppedFile;
+    });
+
     //for web, import dart:html
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -43,21 +66,50 @@ class _CreateAccountViewState extends State<CreateAccountView> {
             padding: const EdgeInsets.all(15.0),
             child: ListView(
               children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: (_imageFile == null ? const AssetImage('assets/3.jpg') : FileImage(_imageFile!)) as ImageProvider
-                      
-                ),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(90),
+                    child: _imageFile == null
+                        ? Image.asset(
+                            'assets/1.jpg',
+                            width: 130,
+                            height: 130,
+                            fit: BoxFit.contain,
+                          )
+                        : Image.file(
+                            _imageFile!,
+                            width: 130,
+                            height: 130,
+                            fit: BoxFit.contain,
+                          )),
                 TextButton.icon(
-                  onPressed: (){
-                    showModalBottomSheet(context: context, builder: (context){
-                      return SizedBox(
-                        height: 100,
-                        child: Column(children: [
-                          TextButton.icon(onPressed: ()=>selectImage(imageSource: ImageSource.camera), icon: const Icon(UniconsLine.camera,), label: const Text('Select from Camera',),),
-
-                          TextButton.icon(
-                                  onPressed: () =>selectImage(imageSource: ImageSource.gallery),
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return SizedBox(
+                            height: 100,
+                            child: Column(
+                              children: [
+                                TextButton.icon(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    selectImage(
+                                        imageSource: ImageSource.camera);
+                                  },
+                                  icon: const Icon(
+                                    UniconsLine.camera,
+                                  ),
+                                  label: const Text(
+                                    'Select from Camera',
+                                  ),
+                                ),
+                                TextButton.icon(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    selectImage(
+                                      imageSource: ImageSource.gallery,
+                                    );
+                                  },
                                   icon: const Icon(
                                     UniconsLine.picture,
                                   ),
@@ -65,33 +117,41 @@ class _CreateAccountViewState extends State<CreateAccountView> {
                                     'Select from Gallery',
                                   ),
                                 ),
-                        ],),
-                      );
-                    });
+                              ],
+                            ),
+                          );
+                        });
                   },
                   icon: const Icon(
                     UniconsLine.camera,
                     color: Colors.grey,
                   ),
-                  label:  Text('Select Profile Picture',
-                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                      color: Colors.grey
-                    ),
+                  label: Text(
+                    'Select Profile Picture',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.grey),
                   ),
-                  
                 ),
-        
                 const SizedBox(
                   height: 35,
                 ),
-      
                 TextFormField(
                   controller: _nameController,
                   keyboardType: TextInputType.name,
                   textCapitalization: TextCapitalization.words,
-                  decoration: const InputDecoration(label: Text('Full Name',),),
+                  decoration: const InputDecoration(
+                    label: Text(
+                      'Full Name',
+                    ),
+                  ),
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'Full Name is required';
+                    }
+                  },
                 ),
-
                 TextFormField(
                   controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
@@ -101,8 +161,17 @@ class _CreateAccountViewState extends State<CreateAccountView> {
                       'Email',
                     ),
                   ),
-                ),
+                  validator: (value) {
 
+                    if (value!.isEmpty) {
+                      return 'Email is required';
+                    }
+                    if (!emailRegExp.hasMatch(value)) {
+                      return 'Email is invalid';
+                    }
+                    
+                  },
+                ),
                 TextFormField(
                   controller: _passwordController,
                   obscureText: true,
@@ -113,20 +182,48 @@ class _CreateAccountViewState extends State<CreateAccountView> {
                       'Password',
                     ),
                   ),
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'Password is required';
+                    }
+                    if(value.length<8){
+                      return 'Password should be 8 characters or more';
+                    }
+                  },
                 ),
-        
                 const SizedBox(
                   height: 25,
                 ),
-
                 TextButton(
-                  onPressed: null, 
+                  onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                      //all good
+                    }else{
+                      //validation failed
+                      Fluttertoast.showToast(
+                          msg: "Please check Input field(s)",
+                          toastLength: Toast.LENGTH_LONG,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    }
+                  },
                   style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).buttonTheme.colorScheme!.background,
+                    backgroundColor:
+                        Theme.of(context).buttonTheme.colorScheme!.background,
                   ),
-                  child: Text('Create Account',style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                    color: Theme.of(context).buttonTheme.colorScheme!.primary,
-                  ),),),
+                  child: Text(
+                    'Create Account',
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          color: Theme.of(context)
+                              .buttonTheme
+                              .colorScheme!
+                              .primary,
+                        ),
+                  ),
+                ),
               ],
             ),
           ),
